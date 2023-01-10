b1b2f06c9c8ed70805bd679274dce02a23fc7de6
Eduardo Fernandez <eduardo.fernandez@accesso.com>
2023-01-09 22:29:54 -0600

    Initial commit

---
+++ app/src/main/java/com/example/testgithubpagesdeploy/Group.kt
@@ -0,0 +1,18 @@
+package com.example.testgithubpagesdeploy
+
+/**
+ * A group of *members*.
+ *
+ * This class has no useful logic; it's just a documentation example.
+ *
+ * @param T the type of a member in this group.
+ * @property name the name of this group.
+ * @constructor Creates an empty group.
+ */
+class Group<T>(val name: String) {
+    /**
+     * Adds a [member] to this group.
+     * @return the new size of the group.
+     */
+    fun add(member: T): Int { return 0 }
+}
